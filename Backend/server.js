b27a9b7c3d6354b48203763cc51736d76c49d9c7
CS50X-RGB <rohanchatterjee866@gmail.com>
2024-02-04 13:07:06 +0530

    Manifest changes

--- Backend/server.js
+++ Backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin:'http://localhost:3000',
+    origin:'https://map-final-git-main-cs50x-rgb.vercel.app',
     methods: ["GET", "POST", "PUT", "DELETE"],
     optionsSuccessStatus: 200,
     credentials: true,
